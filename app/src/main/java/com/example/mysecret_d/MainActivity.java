05e64eaa167131c9a005530aa58fab494653301e
unknown <gvepkhvadze@gmail.com>
2020-03-13 13:38:22 +0400

    AAA

--- app/src/main/java/com/example/mysecret_d/MainActivity.java
+++ app/src/main/java/com/example/mysecret_d/MainActivity.java
@@ -4,9 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,23 +23,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/bpg-banner-caps-webfont.ttf");
         TextView  main_header = (TextView)findViewById(R.id.main_header);
-      //  TextView  app_name = (TextView)findViewById(R.id.app_name);
         EditText  username_input = (EditText)findViewById(R.id.username_input);
         EditText  password_input = (EditText)findViewById(R.id.password_input);
-     //   Button login_button = (Button)findViewById(R.id.login_button);
-
-
-
-        Typeface custom_font = Typeface.createFromAsset(getAssets(),  "fonts/bpg-banner-caps-webfont.ttf");
-
-
         main_header.setTypeface(custom_font);
-       // app_name.setTypeface(custom_font);
         username_input.setTypeface(custom_font);
         password_input.setTypeface(custom_font);
-        //login_button.setTypeface(custom_font);
+
+
+    }
+
+    public void login(View v){
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://www.google.com";
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("Passuxi", response.substring(0,500));
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("shecdoma", error.toString());
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
 
     }
 }
